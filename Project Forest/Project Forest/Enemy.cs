81d6ae4405ceedfc536ab5fe65c257b2dcda7370
James Spinelli <DrX250@hotmail.com>
2015-03-23 19:20:12 -0400

    I accidentally used system.drawing's rectangle class instead of monogame's

--- Project Forest/Project Forest/Enemy.cs
+++ Project Forest/Project Forest/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Drawing;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Project_Forest
