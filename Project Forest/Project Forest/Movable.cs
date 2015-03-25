684ceaa6d310eabd5e3f20a15e0373e2b466aef4
CyberCommander <pokemon_commando@yahoo.com>
2015-03-25 19:09:28 -0400

    Fix up and Main character
    Fixed up some issues with the interface and abstract classes as well as
    started the main character class

--- Project Forest/Project Forest/Movable.cs
+++ Project Forest/Project Forest/Movable.cs
@@ -2,28 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Project_Forest
 {
     abstract class Movable : IEntity
     {
+        //properlly implemented IEntity Interface
+        int x;
+        int y;
+        Rectangle coRect;
         int direction;
         int speed;
 
-        int Direction
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Rectangle CoRect
+        {
+            get { return coRect; }
+            set { coRect = value; }
+        }
+
+        public int Direction
         {
             get { return direction; }
             set { direction = value; }
         }
 
-        int Speed
+        public int Speed
         {
             get { return speed; }
             set { speed = value; }
         }
-        public abstract void Move()
+
+        public Movable(int xpos, int ypos, Rectangle rect, int direct, int fast)//Added constructor so that it can be base called in children
         {
-        
+            x = xpos;
+            y = ypos;
+            coRect = rect;
+            direction = direct;
+            speed = fast;
+        }
+
+        public abstract void Move();
+
+        public virtual bool IsColliding(IEntity entity)
+        {
+            if (coRect.Intersects(entity.CoRect))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
