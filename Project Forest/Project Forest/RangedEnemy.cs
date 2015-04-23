83d4ea7cf52bf363e49f586ab15b6333d0c14bc0
CyberCommander <pokemon_commando@yahoo.com>
2015-04-23 19:12:09 -0400

    Added RangeEnemy
    Put in new RangedEnemy name can be decided later as well as the
    EnemyBullet. Not all methods may work as intended but there is some code
    for all of them

---
+++ Project Forest/Project Forest/RangedEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project_Forest
+{
+    class RangedEnemy:Enemy
+    {
+        private EnemyBullet bullet;
+
+        public EnemyBullet Bullet { get { return bullet; } set { bullet = value; } }
+        public RangedEnemy(int xpos, int ypos, Rectangle rect, Texture2D text, int direct, int speed, int health, Rectangle range, EnemyBullet bull)
+            :base(xpos,ypos,rect,text,direct,speed,health,range)
+        {
+            bullet = bull;
+        }
+
+        public override void Move() { }
+
+        public override void Move(MainCharacter player)
+        {
+            if (X > player.X && player.CoRect.Intersects(AtkRanRect) != true)
+            {
+                X--;
+            }
+            if (X < player.X && player.CoRect.Intersects(AtkRanRect) != true)
+            {
+                X++;
+            }
+        }
+
+        public void Fire(MainCharacter player)
+        {
+            if (player.CoRect.Intersects(AtkRanRect))
+            {
+                bullet = new EnemyBullet(X, Y, bullet.CoRect, bullet.Texture, Direction,8,2);
+            }
+        }
+    }
+}
