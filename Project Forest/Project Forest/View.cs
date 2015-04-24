8c71a1b56db369a4149c51741109a74f3736205e
Matthew Pressman <pressman@outlook.com>
2015-04-24 11:43:31 -0400

    Bringing Model-View-Controller in Manually
    Manually merging over Model-View-Controller because it wasn't working
    before, and was syncing without me telling it to.

--- Project Forest/Project Forest/View.cs
+++ Project Forest/Project Forest/View.cs
@@ -11,12 +11,31 @@ namespace Project_Forest
     {
         List<IEntity> entities; //List of entities to draw to the screen
         ViewStates state; //The current state of the view
+        Rectangle currentLevelView;
 
         //Accessor and mutator for state
         public ViewStates State
         {
             get { return state; }
             set { state = value; }
+        }
+
+        public Rectangle CurrentLevelView
+        {
+            get { return currentLevelView; }
+            set { currentLevelView = value; }
+        }
+
+        public int X
+        {
+            get { return currentLevelView.X; }
+            set { currentLevelView.X = value; }
+        }
+
+        public int Y
+        {
+            get { return currentLevelView.Y; }
+            set { currentLevelView.Y = value; }
         }
 
         //Constructor that makes objects of the fields
@@ -43,8 +62,55 @@ namespace Project_Forest
                 if (entity is Movable)
                 {
                     Movable movable = entity as Movable;
+                    if (movable is Damaging)
+                    {
+                        Damaging damaging = movable as Damaging;
 
-                    if (movable.Direction == 0)
+                        if (damaging.Active)
+                        {
+                            if (damaging is ChainSaw)
+                            {
+                                ChainSaw chain = damaging as ChainSaw;
+
+                                Rectangle sourceRect = new Rectangle(0, 0, chain.Texture.Width, chain.Texture.Height);
+                                if (chain.Direction == 1)
+                                {
+                                    sb.Draw(chain.Texture, chain.CoRect, sourceRect, Color.White, chain.Rotation, new Vector2(0, 0), SpriteEffects.FlipHorizontally, 0f);
+                                }
+                                else
+                                {
+                                    sb.Draw(chain.Texture, chain.CoRect, sourceRect, Color.White, chain.Rotation, new Vector2(0, 0), SpriteEffects.None, 0f);
+                                }
+                            }
+                            else 
+                            {
+                                sb.Draw(damaging.Texture, damaging.CoRect, Color.White);
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else if (movable.Direction == 1)
+                    if (movable is Enemy)
+                    {
+                        Enemy enemy = movable as Enemy;
+                        if (enemy is Ent)
+                        {
+                            Ent ent = enemy as Ent;
+                            if (ent.Direction == 0)
+                            {
+                                sb.Draw(ent.Texture, ent.CoRect, Color.White);
+                            }
+                            else
+                            {
+                                Rectangle sourceRect = new Rectangle(0, 0, ent.Texture.Width, ent.Texture.Height);
+                                sb.Draw(ent.Texture, ent.CoRect, sourceRect, Color.White, 0.0f, Vector2.Zero, SpriteEffects.FlipHorizontally, 0);
+                            }
+                        }
+                    }
+                    else if (movable.Direction == 0)
                     {
                         sb.Draw(movable.Texture, movable.CoRect, Color.White);
                     }
