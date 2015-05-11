a7f6217f5c277c2231ee9bc92a42f3a8a98db162
Matthew Pressman <pressman@outlook.com>
2015-05-10 21:37:43 -0400

    Merged Children into Master
    Brought in all of the files James has been working on in children into
    Master

---
+++ Project Forest/EnemyBullet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project_Forest
+{
+    class EnemyBullet:Damaging
+    {
+
+        public EnemyBullet(int xpos, int ypos, Rectangle rect, Texture2D text, int direct, int speed, int pwr)
+            : base(xpos, ypos, rect, text, direct, speed, pwr)
+        {
+            Active = true;
+        }
+        public override void Move()
+        {
+            if(Direction == 0)
+            {
+                X += Speed;
+            }
+            if(Direction == 1)
+            {
+                X -= Speed;
+            }
+        }
+
+        public override void doDamage(Enemy e){}            
+
+        public void doDamage(MainCharacter player)
+        {
+            if(CoRect.Intersects(player.CoRect) && Active == true)
+            {
+                player.HP -= Dmg;
+                Active = false;
+            }
+        }
+    }
+}
---
+++ Project Forest/FlyingEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project_Forest
+{
+    class FlyingEnemy:RangedEnemy
+    {
+        public FlyingEnemy(int xpos, int ypos, Rectangle rect, Texture2D text, int direct, int speed, int health, Rectangle range, EnemyBullet bull)
+            : base(xpos,ypos,rect,text,direct,speed,health,range,bull)
+        {
+
+        }
+
+        public override void Move() { }
+
+        public override void Move(MainCharacter player)
+        {
+            if (X > player.X)
+            {
+                X -= Speed;
+            }
+            if (X < player.X)
+            {
+                X += Speed;
+            }
+        }
+
+        public void Fire(MainCharacter player)
+        {
+            if (player.X + 5 == X || player.X - 5 == X)//Deteremine best way
+            {
+                Bullet = new EnemyBullet(X, Y, Bullet.CoRect, Bullet.Texture, 1, 8, 2);//Note decide what number direction is down
+            }
+        }
+    }
+}
