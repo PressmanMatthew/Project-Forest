da22ac7c260a99211b1d0f922b258f3dbee06226
James Spinelli <DrX250@hotmail.com>
2015-05-18 10:58:45 -0400

    Added Zuul

---
+++ Project Forest/BossZuul.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Project_Forest
+{
+    class BossZuul 
+    {
+        int x;
+        int y;
+
+        int X 
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public BossZuul(int ix, int iy)
+        {
+            x = ix;
+            y = iy;
+            g = new GameTime();
+        }
+
+        public bool DuelZuul()
+        {
+
+        }
+    }
+}
